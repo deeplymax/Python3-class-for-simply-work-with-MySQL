089650fc5a6eaeb141c1295ce3c232d1e327a81b
deeplymax <deeplymax@gmail.com>
2019-01-26 18:42:30 +0400

    develop a connecto to db, simple query, select and insert

---
+++ MYSQL.py
@@ -0,0 +1,77 @@
+import mysql.connector
+
+class MYSQL:
+    host = "localhost"
+    user = "root"
+    password = ""
+    databaseName =  "myDatabase"
+    database = mysql.connector.connect()
+
+
+    def __init__(self, host, user, password, databaseName):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.databaseName = databaseName
+
+
+    def connect(self):
+        """
+        :return: mysql.connector object
+        """
+        self.database = mysql.connector.connect(
+            host = self.host,
+            user = self.user,
+            passwd = self.password,
+            database = self.databaseName
+        )
+        return self.database
+
+
+    def query(self, query, commit=False):
+        """
+        :param query: query string
+        :param commit: Flag on whether to commit after the request
+        :return: cursor object
+        """
+        mycursor = self.database.cursor()
+        mycursor.execute(query)
+        if(commit):
+            self.database.commit()
+        return mycursor
+
+
+    def select(self, table, *args):
+        """
+        :param table: table in db
+        :param args: colums which need to select
+        :return: json (column : value)
+        """
+        genQ = "SELECT {} FROM {}".format(','.join(['`' + str(it) + '`' for it in args]),  table)
+        print(genQ)
+        mycursor = self.query(genQ)
+        q = mycursor.fetchall()
+
+        #to JSON
+        names = [i[0] for i in mycursor.description]
+        jsonAnswer = []
+        for it in range(len(q)):
+            jsonAnswer.append(dict())
+            for x in range(len(names)):
+                jsonAnswer[it][names[x]] = q[it][x]
+        return jsonAnswer
+
+
+    def insert(self, table, **kwargs):
+        """
+        :param table: table in db
+        :param kwargs: columnName = Value
+        :return: ok or exception
+        """
+        columns = [it for it in kwargs]
+        values = [kwargs[it] for it in kwargs]
+        q = "INSERT INTO {} ({}) VALUES ({})".format('`' + table + '`',
+                                                     ','.join(['`' + str(it) + '`' for it in columns]),
+                                                     ','.join('\'' + str(it) + '\'' for it in values))
+        self.query(q, commit=True)
+        return "Ok! {}".format(q)
